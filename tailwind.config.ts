0c723b50952e12611fcf9d1884f0cc997ac06d52
Khairul Habib Bin Samaon <57700102+khairul35@users.noreply.github.com>
2024-01-03 09:28:59 +0800

    setup tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,34 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundColor: {
+        'bg-color': '#CEE1EE', // Define a custom background color class
+      },
+      button: {
+        'primary': '#158BB8',
+      },
+      fontWeight: {
+        'label': '600',
+      },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#158BB8",
+          "secondary": "#d926a9",
+          "accent": "#1fb2a6",
+          "neutral": "#2a323c",
+          "base-100": "#1d232a",
+          "info": "#3abff8",
+          "success": "#36d399",
+          "warning": "#fbbd23",
+          "error": "#f87272",
+        },
+      },
+    ],
+  },
 }
-export default config
+export default config;
