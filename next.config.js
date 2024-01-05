0fbfd406b5d670c5599accfb4e60e2e8e2de343a
Khairul Habib Bin Samaon <57700102+khairul35@users.noreply.github.com>
2024-01-05 08:52:34 +0800

    update export static

--- next.config.js
+++ next.config.js
@@ -1,28 +1,16 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     async generateStaticParams() {
-//         return {
-//             '/': { page: '/' },
-//             '/login': { page: '/login' },
-//             '/home': { page: '/home' },
-//             '/agents': { page: '/agents' },
-//             '/merchants': { page: '/merchants' },
-//             '/users': { page: '/users' },
-//             '/settings': { page: '/settings' },
-//         };
-//     },
-// }
 const nextConfig = {
-  output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+    async generateStaticParams() {
+        return {
+            '/': { page: '/' },
+            '/login': { page: '/login' },
+            '/home': { page: '/home' },
+            '/agents': { page: '/agents' },
+            '/merchants': { page: '/merchants' },
+            '/users': { page: '/users' },
+            '/settings': { page: '/settings' },
+        };
+    },
 }
  
 module.exports = nextConfig
